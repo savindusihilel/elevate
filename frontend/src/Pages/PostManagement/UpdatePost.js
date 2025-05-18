17815badf8b9463d2a865e5f307c05d186559ee6
Sanduni2002 <sandunihansimagunasinghe.com>
2025-05-18 16:39:14 +0530

    load post data by ID and initialize form state

--- frontend/src/Pages/PostManagement/UpdatePost.js
+++ frontend/src/Pages/PostManagement/UpdatePost.js
@@ -4,6 +4,9 @@ import axios from 'axios';
 import { FaTrash } from 'react-icons/fa';
 import NavBar from '../../Components/NavBar/NavBar';
 
+//
+
+
 function UpdatePost() {
   const { id } = useParams();
   const navigate = useNavigate();
